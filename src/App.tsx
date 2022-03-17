be98cac0cf45fc7735287baa28e61d0b07d247d1
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-03-16 23:44:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="flex items-center justify-center">
+        <h1 className="text-2xl">Calcula tu promedio</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
