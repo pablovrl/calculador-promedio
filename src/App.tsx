a2c1657393b28623b29c5f0aef19dd5ba394aa3e
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-03-29 19:08:12 -0300

    change react-responsive query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useState } from "react";
 import Nota from "./components/Nota";
 import { useMediaQuery } from "react-responsive";
+import { motion } from "framer-motion";
 
 interface Nota {
   id: number;
@@ -18,7 +19,7 @@ function App() {
     { id: 1, nota: "", porcentaje: "" },
   ]);
 
-  const isDesktop = useMediaQuery({ query: "(min-width: 1224px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
 
   const addNewNota = () => {
     const lastId = notas[notas.length - 1].id;
@@ -81,29 +82,29 @@ function App() {
     <>
       <div className="background h-full w-full absolute"></div>
       <div className="flex items-center justify-center relative z-10">
-        <div className="flex items-center justify-center flex-col w-10/12 pt-10 md:max-w-6xl">
-          <h1 className="text-4xl font-bold mb-5 text-center md:mb-16">
+        <div className="flex items-center justify-center flex-col w-10/12 pt-10 lg:max-w-6xl">
+          <h1 className="text-4xl font-bold mb-5 text-center lg:mb-16 lg:text-6xl">
             Calcula tu promedio de notas 📚
           </h1>
-          <div className="w-full flex flex-col items-center justify-center md:flex-row">
+          <div className="w-full flex flex-col items-center justify-center lg:flex-row">
             <button
-              className="rounded-md w-2/3 h-10 bg-blue-500 text-white mb-1 md:m-0 md:w-1/4 md:mr-5"
+              className="rounded-lg w-2/3 h-10 bg-blue-500 text-white mb-1 lg:m-0 lg:w-1/4 lg:mr-5 lg:text-xl "
               onClick={addNewNota}
             >
               Agregar nota
             </button>
             <button
-              className="rounded-md w-2/3 h-10 bg-red-500 text-white mb-5 md:m-0 md:w-1/4"
+              className="rounded-lg w-2/3 h-10 bg-red-500 text-white mb-5 lg:m-0 lg:w-1/4 lg:text-xl "
               onClick={deleteLastNota}
             >
               Eliminar última nota
             </button>
           </div>
-          <div className="flex flex-col justify-center items-center md:flex-row md:mt-5">
+          <div className="flex flex-col justify-center items-center lg:flex-row lg:mt-5 lg:text-xl">
             <p>Ingresa tus notas sin comas ni puntos.</p>
-            <p className="md:ml-1">(ej: 50, 65, 48).</p>
+            <p className="lg:ml-1">(ej: 50, 65, 48).</p>
           </div>
-          <div className="flex w-full mt-4 mb-24 md:w-2/3 ">
+          <div className="flex w-full mt-10 mb-24 lg:w-2/3 ">
             <div className="w-full">
               {notas.map((nota, i) => (
                 <Nota
@@ -119,8 +120,8 @@ function App() {
             {isDesktop && (
               <div className="flex flex-col items-center w-full">
                 <div className="text-center">
-                  <h1>Promedio de notas</h1>
-                  <h1 className="text-6xl">{notaFinal.toFixed(1)}</h1>
+                  <h1 className="text-2xl">Promedio de notas</h1>
+                  <h1 className="text-7xl">{notaFinal.toFixed(1)}</h1>
                 </div>
               </div>
             )}
@@ -128,7 +129,7 @@ function App() {
         </div>
       </div>
       {!isDesktop && (
-        <div className="fixed bottom-0 bg-gray-300 border-t-gray-500 border-t-4 w-full h-20 flex flex-col justify-center items-center font-bold">
+        <div className="fixed bottom-0 bg-gray-300 border-t-gray-500 border-t-4 w-full h-20 flex flex-col justify-center items-center font-bold z-10">
           <p>Promedio de notas</p>
           <p>{notaFinal.toFixed(1)}</p>
         </div>
