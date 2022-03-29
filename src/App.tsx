e440224029bb6cd85b08d1bd0ea3633862ea8aab
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-03-29 15:19:31 -0300

    update web view using react-responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Nota from "./components/Nota";
+import { useMediaQuery } from "react-responsive";
 
 interface Nota {
   id: number;
@@ -16,6 +17,8 @@ function App() {
     },
     { id: 1, nota: "", porcentaje: "" },
   ]);
+
+  const isDesktop = useMediaQuery({ query: "(min-width: 1224px)" });
 
   const addNewNota = () => {
     const lastId = notas[notas.length - 1].id;
@@ -76,43 +79,59 @@ function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center">
-        <div className="flex items-center justify-center flex-col w-10/12 pt-10">
-          <h1 className="text-4xl font-bold mb-5 text-center">
+      <div className="flex items-center justify-center  ">
+        <div className="flex items-center justify-center flex-col w-10/12 pt-10 md:max-w-6xl">
+          <h1 className="text-4xl font-bold mb-5 text-center md:mb-16">
             Calcula tu promedio de notas 📚
           </h1>
-          <button
-            className="rounded-md w-2/3 h-10 bg-blue-500 text-white mb-1"
-            onClick={addNewNota}
-          >
-            Agregar nota
-          </button>
-          <button
-            className="rounded-md w-2/3 h-10 bg-red-500 text-white mb-5"
-            onClick={deleteLastNota}
-          >
-            Eliminar última nota
-          </button>
-          <p>Ingresa tus notas sin comas ni puntos.</p>
-          <p>(ej: 50, 65, 48).</p>
-          <div className="w-full mt-2 mb-24">
-            {notas.map((nota, i) => (
-              <Nota
-                key={i}
-                id={nota.id}
-                nota={nota.nota}
-                porcentaje={nota.porcentaje}
-                handleNotaChange={handleNotaChange}
-                handlePorcentajeChange={handlePorcentajeChange}
-              />
-            ))}
+          <div className="w-full flex flex-col items-center justify-center md:flex-row">
+            <button
+              className="rounded-md w-2/3 h-10 bg-blue-500 text-white mb-1 md:m-0 md:w-1/4 md:mr-5"
+              onClick={addNewNota}
+            >
+              Agregar nota
+            </button>
+            <button
+              className="rounded-md w-2/3 h-10 bg-red-500 text-white mb-5 md:m-0 md:w-1/4"
+              onClick={deleteLastNota}
+            >
+              Eliminar última nota
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center md:flex-row md:mt-5">
+            <p>Ingresa tus notas sin comas ni puntos.</p>
+            <p className="md:ml-1">(ej: 50, 65, 48).</p>
+          </div>
+          <div className="flex w-full mt-4 mb-24 md:w-2/3 ">
+            <div className="w-full">
+              {notas.map((nota, i) => (
+                <Nota
+                  key={i}
+                  id={nota.id}
+                  nota={nota.nota}
+                  porcentaje={nota.porcentaje}
+                  handleNotaChange={handleNotaChange}
+                  handlePorcentajeChange={handlePorcentajeChange}
+                />
+              ))}
+            </div>
+            {isDesktop && (
+              <div className="flex flex-col items-center w-full">
+                <div className="text-center">
+                  <h1>Promedio de notas</h1>
+                  <h1 className="text-6xl">{notaFinal.toFixed(1)}</h1>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      <div className="fixed bottom-0 bg-gray-300 border-t-gray-500 border-t-4 w-full h-20 flex flex-col justify-center items-center font-bold">
-        <p>Promedio de notas</p>
-        <p>{notaFinal.toFixed(1)}</p>
-      </div>
+      {!isDesktop && (
+        <div className="fixed bottom-0 bg-gray-300 border-t-gray-500 border-t-4 w-full h-20 flex flex-col justify-center items-center font-bold">
+          <p>Promedio de notas</p>
+          <p>{notaFinal.toFixed(1)}</p>
+        </div>
+      )}
     </>
   );
 }
